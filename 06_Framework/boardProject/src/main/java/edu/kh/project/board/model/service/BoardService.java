95176a82b8b44de92ce22367be713c3a36ba5b5b
DESKTOP-GKGL0N0\J <grainbite@gmail.com>
2023-05-09 00:33:42 +0900

    230508 게시판 페이지네이션

--- 06_Framework/boardProject/src/main/java/edu/kh/project/board/model/service/BoardService.java
+++ 06_Framework/boardProject/src/main/java/edu/kh/project/board/model/service/BoardService.java
@@ -3,17 +3,15 @@ package edu.kh.project.board.model.service;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.stereotype.Service;
-
-
-
 public interface BoardService {
 
-	/** 게시판 종류 목록 조회
-	 * @return
-	 */
 	List<Map<String, Object>> selectBoardTypeList();
-	
 
-	
+	/** 게시글 목록 조회
+	 * @param boardCode
+	 * @param cp
+	 * @return map
+	 */
+	Map<String, Object> selectBoardList(int boardCode, int cp);
+
 }
